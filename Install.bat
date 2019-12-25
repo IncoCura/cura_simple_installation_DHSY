814d91dbd756e1084fd918b060ee21dccf9c45b7
KangDroid <hyunwoo286@gmail.com>
2019-12-26 00:40:40 +0900

    Add Basic Installation Script

---
+++ Install.bat
@@ -0,0 +1,3 @@
+copy .\Extruder_Def\*.json "C:\Program Files\Ultimaker Cura 4.4\resources\extruders"
+copy .\Printer_Def\*.json "C:\Program Files\Ultimaker Cura 4.4\resources\definitions"
+copy .\corem_platform.stl "C:\Program Files\Ultimaker Cura 4.4\resources\meshes"
